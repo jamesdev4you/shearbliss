24458bb659e04c41463a359d519730a666cb280a
James <jamesdev4you@gmail.com>
2023-11-15 17:29:29 -0500

    npm install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Home from './PageComponents/Home/home.js';
+import Gallery from './PageComponents/Gallery/gallery.js';
+import Appointment from '../src/PageComponents/Appointment/appointment.js';
+import Reviews from './PageComponents/Reviews/reviews.js';
+import Team from './PageComponents/Team/team.js';
+import NavBar from './PageComponents/NavBar/navbar.js';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Routes, Route } from 'react-router-dom';
+import { MyServiceButton } from '../src/PageComponents/styledComponents.js';
+import client from './sanityClient';
+
+const query = '*[_type == "header"]';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1B1E1E',
+    },
+    secondary: {
+      main: '#E4DCC0',
+    },
+  },
+});
 
 function App() {
+  const [headerOptions, setHeaderOptions] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch(query)
+      .then((queryResponse) => {
+        console.log('Query Response', queryResponse);
+        const updatedHeaderOptions = queryResponse.map((item) => {
+          return {
+            background: item.backgroundImage.asset._ref,
+            title: item.heading,
+            desc: item.tagline,
+            Button: MyServiceButton,
+          };
+        });
+        setHeaderOptions(updatedHeaderOptions);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path='/' element={<NavBar />} color='secondary'>
+          <Route path='/' element={<Home headerOp={headerOptions[1]} />} />
+          <Route
+            path='/appointment'
+            element={<Appointment headerOp={headerOptions[2]} />}
+          />
+          <Route
+            path='/reviews'
+            element={<Reviews headerOp={headerOptions[4]} />}
+          />
+          <Route path='/team' element={<Team headerOp={headerOptions[3]} />} />
+          <Route
+            path='/gallery'
+            element={<Gallery headerOp={headerOptions[0]} />}
+          />
+          <Route path='*' element={<Home />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
