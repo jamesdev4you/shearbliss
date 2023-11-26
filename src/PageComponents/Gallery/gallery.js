98dba8ea321035615aadd1f829f876749b90f4c7
James <jamesdev4you@gmail.com>
2023-11-26 16:40:21 -0500

    final mvp

--- src/PageComponents/Gallery/gallery.js
+++ src/PageComponents/Gallery/gallery.js
@@ -1,103 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import imageUrlBuilder from '@sanity/image-url';
+import client from '../../sanityClient';
 import HomeHeader from '../Home/homeHeader';
 import NavBar from '../NavBar/navbar';
-import homeImage from '../assets/headerhome.jpg';
-import contactImage from '../assets/headercontact.jpg';
-import headerReviews from '../assets/headerReviews.jpg';
-import headerTeam from '../assets/headerTeam.jpg';
-import headerGallery from '../assets/headerGallery.jpg';
-import Woman from '../assets/woman.jpg';
-import WomanTwo from '../assets/woman2.jpg';
-import WomanThree from '../assets/woman3.jpg';
-import WomanFour from '../assets/woman4.jpg';
-import WomanFive from '../assets/woman5.jpg';
-import Twitter from '../assets/twitter.png';
-import Youtube from '../assets/youtube.png';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
 import { Box, Typography } from '@mui/material';
 import { MyServiceButton } from '../styledComponents.js';
 import Footer from '../Footer/footer.js';
 
-const itemData = [
-  {
-    img: homeImage,
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: contactImage,
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: headerReviews,
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: headerTeam,
-    title: 'Coffee',
-    author: '@nolanissac',
-    cols: 2,
-  },
-  {
-    img: headerGallery,
-    title: 'Hats',
-    author: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: Woman,
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: WomanTwo,
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: WomanThree,
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: WomanFour,
-    title: 'Mushrooms',
-    author: '@silverdalex',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: WomanFive,
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: Twitter,
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: Youtube,
-    title: 'Bike',
-    author: '@southside_customs',
-    cols: 2,
-  },
-];
+const query = '*[_type == "galleryPicture"]';
 
-const gallery = (props) => {
+const Gallery = (props) => {
+  const [pictureOptions, setPictureOptions] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch(query)
+      .then((queryResponse) => {
+        console.log('Response', queryResponse);
+        const updatedPictureOptions = queryResponse.map((item) => {
+          return {
+            picture: item?.image?.asset?._ref,
+            title: item?.image?.alt,
+          };
+        });
+        setPictureOptions(updatedPictureOptions);
+      })
+      .catch(console.error);
+  }, []);
+
+  const builder = imageUrlBuilder(client);
+
+  function urlFor(source) {
+    return builder.image(source);
+  }
+
+  console.log('hiiii', pictureOptions);
+
   return (
     <div>
       <NavBar />
@@ -119,7 +60,7 @@ const gallery = (props) => {
             marginBottom: '15px',
             fontFamily: 'Birds',
             paddingTop: '.8em',
-            fontSize: { lg: '98px', md: '64px', sm: '64px', xs: '54px'  },
+            fontSize: { lg: '98px', md: '64px', sm: '64px', xs: '54px' },
           }}
           color='secondary.main'
         >
@@ -139,20 +80,18 @@ const gallery = (props) => {
             border: '1px solid #E4DCC0',
           }}
         >
-          {itemData.map((item) => (
-            <ImageListItem key={item.img}>
+          {pictureOptions.map((item) => (
+            <ImageListItem key={item.title}>
               <img
-                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={urlFor(item.picture)}
+                src={urlFor(item.picture)}
                 alt={item.title}
                 loading='lazy'
               />
               <ImageListItemBar
-                title={item.title}
-                subtitle={item.author}
                 actionIcon={<MyServiceButton />}
                 sx={{
-                  paddingRight: '15px',
+                  padding: '15px',
                   display: {
                     xs: 'none',
                     sm: 'none',
@@ -171,4 +110,4 @@ const gallery = (props) => {
   );
 };
 
-export default gallery;
+export default Gallery;
